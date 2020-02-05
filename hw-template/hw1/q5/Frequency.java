7d04b9f3c237637bbbd9a2cdd7d27e515edf3793
thienson-ho <thiensonho@utexas.edu>
2020-02-05 14:06:25 -0600

    Adjusted our solution for q5 to match template.

--- hw-template/hw1/q5/Frequency.java
+++ hw-template/hw1/q5/Frequency.java
@@ -1,7 +1,60 @@
 package q5;
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
 
 public class Frequency implements Callable<Integer> {
+    int start;
+    int end;
+    int x;
+    int count;
+    int[] array;
+
+    public Frequency(int start, int end, int x, int[] array) {
+        this.start = start;
+        this.end = end;
+        this.x = x;
+        this.array = array;
+        this.count = 0;
+    }
+
     public static int parallelFreq(int x, int[] A, int numThreads){
         //your implementation goes here, return -1 if the input is not valid.
-    }  
+        int start = 0;
+        int end = A.length / numThreads;
+        int count = 0;
+        FutureTask[] threads = new FutureTask[numThreads];
+
+        for(int i = 0; i < numThreads; i++) {
+
+            Callable<Integer> callable = new Frequency(start, end, x, A);
+            start = end;
+            end += A.length/numThreads;
+
+            if(i == numThreads - 2) {
+                end = A.length;
+            }
+
+            threads[i] = new FutureTask(callable);
+            Thread t = new Thread(threads[i]);
+            t.start();
+        }
+        
+        for(int i = 0; i < numThreads; i++) {
+            try {
+                count += (int) threads[i].get();
+            } catch (Exception e) {
+                System.out.print(e);
+            }
+        }
+
+        return count;
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        for(int i = start; i < end; i++) {
+            if(array[i] == x) count++;
+        }
+        return count;
+    }
 }
