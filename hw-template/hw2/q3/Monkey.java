2906a232b8d233611ca94e7f34cd396ed9c1a820
Rishab Chander <rishab.chander@gmail.com>
2020-02-16 15:37:21 -0600

    problem 3 working

--- hw-template/hw2/q3/Monkey.java
+++ hw-template/hw2/q3/Monkey.java
@@ -1,17 +1,52 @@
 package q3;
 
+import java.util.concurrent.Semaphore;
+
 public class Monkey {
 
-    public Monkey() {
+    static int currDirection;
+    static int count;
+    static Semaphore rope;
 
+    public Monkey() {
+        currDirection = 0;
+        count = 0;
+        rope = new Semaphore(3, true);
     }
 
     public void ClimbRope(int direction) throws InterruptedException {
+        
+        // System.out.println(currDirection);
+        // System.out.println("direction requested: " + direction);
+        // System.out.println("available permits: " + rope.availablePermits());
+        if(direction == -1) {
+            rope.acquire(3);
+            currDirection = -1;
+        }
 
+        else if (direction == currDirection || rope.availablePermits() == 3) {
+            //System.out.println("acquiring the rope");
+            currDirection = direction;
+            
+            rope.acquire();
+        }
+
+        else {
+            while (rope.availablePermits() != 3) {
+            }
+            currDirection = direction;
+            rope.acquire();
+        }
     }
 
     public void LeaveRope() {
+        if(currDirection == -1) {
+            rope.release(3);
+        } 
 
+        else {
+            rope.release();
+        }
     }
 
     /**
@@ -19,12 +54,12 @@ public class Monkey {
      *
      * @return the number of monkeys on the rope
      *
-     * Positive Test Cases:
-     * case 1: when normal monkey (0 and 1) is on the rope, this value should <= 3, >= 0
-     * case 2: when Kong is on the rope, this value should be 1
+     *         Positive Test Cases: case 1: when normal monkey (0 and 1) is on the
+     *         rope, this value should <= 3, >= 0 case 2: when Kong is on the rope,
+     *         this value should be 1
      */
     public int getNumMonkeysOnRope() {
-        return -1;
+        return 3 - rope.availablePermits();
     }
 
 }
