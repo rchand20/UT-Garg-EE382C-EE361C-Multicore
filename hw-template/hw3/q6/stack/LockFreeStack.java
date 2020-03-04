2188242608cae93add98a9b8c5fc3521892e1e7d
rchand20 <rchand20@utexas.edu>
2020-03-03 22:51:46 -0600

    implemented stack

--- hw-template/hw3/q6/stack/LockFreeStack.java
+++ hw-template/hw3/q6/stack/LockFreeStack.java
@@ -1,20 +1,51 @@
 package q6.stack;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 public class LockFreeStack implements MyStack {
     // you are free to add members
+    AtomicReference<Node> top;
 
     public LockFreeStack() {
         // implement your constructor here
+        top = new AtomicReference<Node>();
+
     }
 
     public boolean push(Integer value) {
         // implement your push method here
-        return false;
+        Node node = new Node(value);
+        while(true) {
+            Node oldTop = top.get();
+            node.next = oldTop;
+            if(top.compareAndSet(oldTop, node)) {
+                return true;
+            }
+            else {
+                Thread.yield();
+            }
+
+            return false;
+        }
+
     }
 
     public Integer pop() throws EmptyStack {
         // implement your pop method here
-        return null;
+        while(true) {
+            Node old = top.get();
+            if(old == null) {
+                throw new EmptyStack();
+            }
+
+            Integer val = old.value;
+            Node newTop = old.next;
+            if(top.compareAndSet(old, newTop)) {
+                return val;
+            } else {
+                Thread.yield();
+            }
+        }
     }
 
     protected class Node {
