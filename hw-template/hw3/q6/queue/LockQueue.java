8fc8d27cfe82be17932c51f3e17b0629460794dd
rchand20 <rchand20@utexas.edu>
2020-03-04 17:08:48 -0600

    Lock Q implemented, to be tested

--- hw-template/hw3/q6/queue/LockQueue.java
+++ hw-template/hw3/q6/queue/LockQueue.java
@@ -1,20 +1,61 @@
 package q6.queue;
 
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class LockQueue implements MyQueue {
     // you are free to add members
+    ReentrantLock enqLock;
+    ReentrantLock deqLock;
+
+    Node head;
+    Node tail;
+
+    AtomicInteger size;
 
     public LockQueue() {
         // implement your constructor here
+        head = new Node(null);
+        tail = head;
+        enqLock = new ReentrantLock();
+        deqLock = new ReentrantLock();
+
     }
+
 
     public boolean enq(Integer value) {
         // implement your enq method here
-        return false;
+        if(value == null) {
+            return false;
+        }
+
+        try {
+            Node e = new Node(value);
+            tail.next = e;
+            tail = e;
+            return true;
+        } finally {
+            enqLock.unlock();
+        }
+
     }
 
     public Integer deq() {
         // implement your deq method here
-        return null;
+        Integer result;
+        deqLock.lock();
+
+        try {
+            if(head.next == null) {
+                return null;
+            }
+
+            result = head.next.value;
+            head = head.next;
+            return result;
+        } finally {
+            deqLock.unlock();
+        }
     }
 
     protected class Node {
